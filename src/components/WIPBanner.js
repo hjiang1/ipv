2440c8436ad0a41694f4dd248982cdd077414732
Sam <hjiang129@gmail.com>
2020-04-24 15:42:10 -0400

    banner tweaks

--- src/components/WIPBanner.js
+++ src/components/WIPBanner.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react"
 import styled from "styled-components"
-import { FaWrench, FaRegTimesCircle } from 'react-icons/fa';
+import { FaTools, FaTimes } from 'react-icons/fa';
 
 const Container = styled.div`
   background-color: red;
@@ -36,18 +36,11 @@ const Container = styled.div`
     min-width: 1.5rem;
     margin-left: 1rem;
     color: black;
-    background-color: white;
-    border-radius: 50%;
-    box-shadow: 0px 0px 0px 1px black inset;
-
-    transition-property: background-color, color, box-shadow;
-    transition-duration: 0.2s;
-    transition-timing-function: ease;
+    padding: 0.2rem;
+    transition: color 0.2s ease;
 
     :hover {
       color: white;
-      background-color: black;
-      box-shadow: 0px 0px 0px 1px white inset;
       cursor: pointer;
     }
   }
@@ -61,15 +54,14 @@ const WIPBanner = () => {
       <Container>
         <div className="content">
           <div className="message">
-            <FaWrench className="wrench-icon" size="2rem" />
+            <FaTools className="wrench-icon" size="2rem" />
             <div>
               <div>This site is currently a work in progress.</div>
               <div>Please contact Sam Jiang at <a href="mailto:hjiang4@partners.org" target="_blank" rel="noopener noreferrer">hjiang4@partners.org</a> to provide feedback on the design of this site.</div>
             </div>
           </div>
-          <FaRegTimesCircle className="close-icon" size="2rem" onClick={() => setOpen(false)} />
+          <FaTimes className="close-icon" size="2rem" onClick={() => setOpen(false)} />
         </div>
-
       </Container>
     )
   } else {
